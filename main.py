8ad8b564fbc5d6edddaf884e7e7347c3d81902e6
sadkloud <112272752+sadklouds@users.noreply.github.com>
2024-09-13 14:19:02 +0100

    added game loop and constants

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
             
         screen.fill((0, 0, 0))
         pygame.display.flip()
 
+        # limit to 60fps
+        dt = clock.tick(60) / 1000
+
+    pygame.quit()
+        
 
 if __name__ == "__main__":
     main()
